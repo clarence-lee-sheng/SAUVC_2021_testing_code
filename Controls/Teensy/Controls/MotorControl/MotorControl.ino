10406628eef97213d41fde2f416f87b13680ba8c
clarence <cl.sheng34@gmail.com>
2021-07-11 14:36:14 +0800

    Updated motor control with pressure sensor reading

--- Controls/Teensy/Controls/MotorControl/MotorControl.ino
+++ Controls/Teensy/Controls/MotorControl/MotorControl.ino
@@ -4,6 +4,10 @@
 #include <Servo.h>
 #include <Wire.h>
 #include "MS5837.h"
+#include <sensor_msgs/FluidPressure.h>
+#include <merlion_hardware/Motor.h>
+
+
 
 MS5837 sensor;
 
@@ -15,9 +19,7 @@ MS5837 sensor;
 #define M5_PIN 15
 
 
-
-
-struct Motor 
+struct MotorSpec 
 {
    int pinNo;
    int armingFreq; 
@@ -25,8 +27,8 @@ struct Motor
 };
 
 
-///Function declaration
-void subscriberCallBack(const std_msgs::UInt16& sub_msg );
+///=============Function declaration
+void motorCommandCallback(const merlion_hardware::Motor& motorVal );
 
 //ROS declarations 
 ros::NodeHandle node_handle; //rosserial object 
@@ -35,16 +37,13 @@ std_msgs::String sub_msg; // ros message
 std_msgs::UInt16 motorState_msg; // motorState string publisher message
 
 ros::Publisher motorState_Publisher("motor_state",&motorState_msg);
-ros::Subscriber<std_msgs::UInt16> motor_Subscriber("motor_command", &subscriberCallBack);
+ros::Subscriber<merlion_hardware::Motor> motor_Subscriber("/merlion_hardware/thruster_values", &motorCommandCallback);
 
-Motor motors[MOTORSIZE];
+MotorSpec motors[MOTORSIZE];
 int motorPins [MOTORSIZE] = {M1_PIN,M2_PIN,M3_PIN,M4_PIN,M5_PIN};
 int armingFreq[MOTORSIZE] = {1480,1480,1480,1480,1480};
 int generalArmingFreq = 1480;
 Servo esc;
-
-
-ros::NodeHandle  nh;
 
 sensor_msgs::FluidPressure pressureData;
 ros::Publisher pressurePub("merlion_hardware/pressure", &pressureData);
@@ -55,9 +54,29 @@ void setup()
   armAll();
   //TODO: find a way to cut off comms when mother controller is not connected
   rosInit();
+  pressureSensorInit();
+}
 
-  nh.initNode();
-  nh.advertise(pressurePub);
+void loop() 
+{
+ //TODO:Update Motor state here
+
+ //TODO:Update Pressure state here
+
+  sensor.read();
+
+  pressureData.fluid_pressure = sensor.pressure() ;  
+//  pressurePub.publish( &pressureData );;
+  Serial.println(sensor.depth());
+  motorState_Publisher.publish(&motorState_msg);
+  node_handle.spinOnce();
+  delay(100);
+}
+
+//============================INIT CODE ==================================
+
+void pressureSensorInit()
+{
   Serial.println("Starting");
   
   Wire.begin();
@@ -72,28 +91,11 @@ void setup()
     Serial.println("\n\n\n");
     delay(5000);
   }
-
+  Serial.println("sensor done");
+  
   sensor.setModel(MS5837::MS5837_30BA);
   sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
 }
-
-void loop() 
-{
- //TODO:Update Motor state here
-
- //TODO:Update Pressure state here
-  sensor.read();
-
-  pressureData.fluid_pressure = sensor.pressure() ;  
-  pressurePub.publish( &pressureData );
-  nh.spinOnce();
- 
-  motorState_Publisher.publish(&motorState_msg);
-  node_handle.spinOnce();
-  delay(100);
-}
-
-//============================INIT CODE ==================================
 void armAll()
 {
   Serial.println("Arming Motors");
@@ -112,129 +114,142 @@ void armAll()
   }
   Serial.println("MOTORS ARMED !!!");
 }
+//
 
 ///============================COMMS CODE ==================================
-void serialEvent()
-{
-  //!WARNING : TEMP CODE REPLACE WITH LOOK UP FUCNTION FOR DYNAMIC COMMS METHOD
-  String readValue = Serial.readString(); 
-  //readValue.flush();
-  int value = readValue.toInt(); 
-  Serial.print("ReadValue : ");Serial.println(readValue);
-  Serial.print("ActualValue : ");Serial.println(value);
-}
+//void serialEvent()
+//{
+//  //!WARNING : TEMP CODE REPLACE WITH LOOK UP FUCNTION FOR DYNAMIC COMMS METHOD
+//  String readValue = Serial.readString(); 
+//  //readValue.flush();
+//  int value = readValue.toInt(); 
+//  Serial.print("ReadValue : ");Serial.println(readValue);
+//  Serial.print("ActualValue : ");Serial.println(value);
+//}
 void rosInit()
 {
   node_handle.initNode();
   node_handle.advertise(motorState_Publisher);
   node_handle.subscribe(motor_Subscriber);
+//  node_handle.advertise(pressurePub);
 }
 ///============================ROS CALLBACK CODE ===================================
 //TODO : Add ROSSERIAL CODE HERE
-void subscriberCallBack(const std_msgs::UInt16& sub_msg )
+void motorCommandCallback(const merlion_hardware::Motor& motorVal )
 {
+  int m1 = motorVal.m1; 
+  int m2 = motorVal.m2;
+  int m3 = motorVal.m3;
+  int m4 = motorVal.m4;
+  int m5 = motorVal.m5;
+
   /// Callback function for motor / pressure sensore return 
-  //TODO: code events for commands
+  motorSetSpeedFreq(motors[0],m1);
+  motorSetSpeedFreq(motors[1],m2);
+  motorSetSpeedFreq(motors[2],m3);
+  motorSetSpeedFreq(motors[3],m4);
+  motorSetSpeedFreq(motors[4],m5);
 }
 
 
 ///============================MOTOR CONTROL CODE ==================================
-void motorSetSpeedAllPercentage(int _speedPercentage [])
-{
-  //TAKE NOTE : MAKE SURE THAT _speedPercentage ARRAY SIZE IS EQUALS TO MOTORSIZE
-  for(int i =0 ;i<MOTORSIZE ; i++)
-  {
-    motors[i].esc.writeMicroseconds(freqConversion(_speedPercentage[i]));  
-    Serial.print("Motor Speed change : Motor : "); Serial.print(i); Serial.print(" speedPercentage : ");Serial.println(_speedPercentage[i]);
-  }
-}
+//void motorSetSpeedAllPercentage(int _speedPercentage [])
+//{
+//  //TAKE NOTE : MAKE SURE THAT _speedPercentage ARRAY SIZE IS EQUALS TO MOTORSIZE
+//  for(int i =0 ;i<MOTORSIZE ; i++)
+//  {
+//    motors[i].esc.writeMicroseconds(freqConversion(_speedPercentage[i]));  
+//    Serial.print("Motor Speed change : Motor : "); Serial.print(i); Serial.print(" speedPercentage : ");Serial.println(_speedPercentage[i]);
+//  }
+//}
 
-void motorSetSpeedFreq(Motor _motor,int _speedFreq)
+void motorSetSpeedFreq(MotorSpec _motor,int _speedFreq)
 {
   //TODO:SCALE THIS UP TO 4 MOTORS FOR EASE OF CONTROL
   _motor.esc.writeMicroseconds(_speedFreq);
   Serial.print("Motor Speed change : ");Serial.println(_speedFreq);
 }
-void motorSetSpeedPercentage(Motor _motor,int _speedPercentage)
-{
-  //TODO:SCALE THIS UP TO 4 MOTORS FOR EASE OF CONTROL 
-  _motor.esc.writeMicroseconds(freqConversion(_speedPercentage));
-  Serial.print("Motor Speed change : ");Serial.println(_speedPercentage);
-}
+//void motorSetSpeedPercentage(MotorSpec _motor,int _speedPercentage)
+//{
+//  //TODO:SCALE THIS UP TO 4 MOTORS FOR EASE OF CONTROL 
+//  _motor.esc.writeMicroseconds(freqConversion(_speedPercentage));
+//  Serial.print("Motor Speed change : ");Serial.println(_speedPercentage);
+//}
 ///============================COMPUTATION CODE ==================================
-int freqConversion(int speedPercentage)
-{
-  if(speedPercentage >= 0)
-  {
-    //CW code : 
-    int freq = map(speedPercentage,0,100,generalArmingFreq,1900 );
-    return freq;
-  }
-  else if(speedPercentage <=0 )
-  {
-    //ACW code : 
-    int freq = map(-speedPercentage,0,100,1100,generalArmingFreq);
-    return freq;
-  }
-  else
-  {
-    //default code 
-    Serial.print("freq :Invalid input : speedPercentage : ");Serial.println(speedPercentage);
-  }
-}
+//int freqConversion(int speedPercentage)
+//{
+//  if(speedPercentage >= 0)
+//  {
+//    //CW code : 
+//    int freq = map(speedPercentage,0,100,generalArmingFreq,1900 );
+//    return freq;
+//  }
+//  else if(speedPercentage <=0 )
+//  {
+//    //ACW code : 
+//    int freq = map(-speedPercentage,0,100,1100,generalArmingFreq);
+//    return freq;
+//  }
+//  else
+//  {
+//    //default code 
+//    Serial.print("freq :Invalid input : speedPercentage : ");Serial.println(speedPercentage);
+//  }
+//}
 
 ///============================TESTING CODE ==================================
-void testSingle(Motor motor)
-{
-  Serial.println("spinning cw");
-  int delayTime = 1000;
-   for(int i =0 ; i<100 ; i+=10 ) 
-   {
-     Serial.println(i);
-     motorSetSpeedPercentage(motor , i);
-     delay(delayTime);
-   }
-   for(int i =100  ; i>0 ; i-=10 ) 
-   {
-    Serial.println(i);
-    motorSetSpeedPercentage(motor , i);
-    delay(delayTime);
-   }
-}
-void test()
-{
-   Serial.println("spinning cw");
-   int delayTime = 1000;
-   for(int i =0  ; i<100 ; i+=1 ) 
-   {
-     Serial.println(i);
-     int speedPercentage[] = {i,i,i,i,i};
-     motorSetSpeedAllPercentage(speedPercentage);
+//void testSingle(MotorSpec motor)
+//{
+//  Serial.println("spinning cw");
+//  int delayTime = 1000;
+//   for(int i =0 ; i<100 ; i+=10 ) 
+//   {
+//     Serial.println(i);
+//     motorSetSpeedPercentage(motor , i);
+//     delay(delayTime);
+//   }
+//   for(int i =100  ; i>0 ; i-=10 ) 
+//   {
+//    Serial.println(i);
+//    motorSetSpeedPercentage(motor , i);
+//    delay(delayTime);
+//   }
+//}
 
-     delay(delayTime);
-   }
-   for(int i =100  ; i>0 ; i-=1 ) 
-   {
-     Serial.println(i);
-     int speedPercentage[] = {i,i,i,i,i}; 
-     motorSetSpeedAllPercentage(speedPercentage);
-     
-     delay(delayTime);
-   }
-   for(int i =0  ; i>-100 ; i-=1 ) 
-   {
-     Serial.println(i);
-     int speedPercentage[] = {i,i,i,i,i};
-     motorSetSpeedAllPercentage(speedPercentage);
-
-     delay(delayTime);
-   }
-   for(int i =-100  ; i<0 ; i+=1 ) 
-   {
-     Serial.println(i);
-     int speedPercentage[] = {i,i,i,i,i}; 
-     motorSetSpeedAllPercentage(speedPercentage);
-     
-     delay(delayTime);
-   }
-}
+//void test()
+//{
+//   Serial.println("spinning cw");
+//   int delayTime = 1000;
+//   for(int i =0  ; i<100 ; i+=1 ) 
+//   {
+//     Serial.println(i);
+//     int speedPercentage[] = {i,i,i,i,i};
+//     motorSetSpeedAllPercentage(speedPercentage);
+//
+//     delay(delayTime);
+//   }
+//   for(int i =100  ; i>0 ; i-=1 ) 
+//   {
+//     Serial.println(i);
+//     int speedPercentage[] = {i,i,i,i,i}; 
+//     motorSetSpeedAllPercentage(speedPercentage);
+//     
+//     delay(delayTime);
+//   }
+//   for(int i =0  ; i>-100 ; i-=1 ) 
+//   {
+//     Serial.println(i);
+//     int speedPercentage[] = {i,i,i,i,i};
+//     motorSetSpeedAllPercentage(speedPercentage);
+//
+//     delay(delayTime);
+//   }
+//   for(int i =-100  ; i<0 ; i+=1 ) 
+//   {
+//     Serial.println(i);
+//     int speedPercentage[] = {i,i,i,i,i}; 
+//     motorSetSpeedAllPercentage(speedPercentage);
+//     
+//     delay(delayTime);
+//   }
+//}
