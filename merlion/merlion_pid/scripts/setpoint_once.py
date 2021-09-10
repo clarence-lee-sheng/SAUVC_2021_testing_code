fdf1e2984c9e8577a66cad4299440ac6af4ff923
clarence <cl.sheng34@gmail.com>
2021-09-10 12:32:43 +0800

    set speed to 0.5

--- merlion/merlion_pid/scripts/setpoint_once.py
+++ merlion/merlion_pid/scripts/setpoint_once.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
         rospy.Publisher('pitch/setpoint', Float64, queue_size=10).publish(Float64(0))
         rospy.Publisher('roll/setpoint',  Float64, queue_size=10).publish(Float64(0))
         # rospy.Publisher('depth/setpoint', Float64, queue_size=10).publish(Float64(0.2))
-        rospy.Publisher('speed/setpoint', Float64, queue_size=10).publish(Float64(2))
+        rospy.Publisher('speed/setpoint', Float64, queue_size=10).publish(Float64(0.5))
         rate.sleep()
         time += 0.1
